349898f1a3a9ae24e1bb1fe11db9c3184561c9e3
Martins Galins <martins.galins@atea.com>
2022-04-07 17:11:27 +0300

    step7 upload.

---
+++ Nodarbiba_2022-04-06_PY1/Patstavigais_darbs/step7.py
@@ -0,0 +1,4 @@
+height = input("Enter the height:")
+radius = input("Enter the radius:")
+print("Volume:",3.14*int(radius)*int(radius)*int(height))
+print("Area:",2*3.14*int(radius)*int(radius)+int(height)*(2*3.14*int(radius)))
