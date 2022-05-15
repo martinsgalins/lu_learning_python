6b79033ce374150868919bae0fc11ff5de95e347
Martins Galins <martins.galins@atea.com>
2022-05-15 09:36:05 +0300

    step1.py upload

---
+++ Nodarbiba_2022-05-11_PY1/Patstavigais_darbs/step1.py
@@ -0,0 +1,12 @@
+users = {
+  "Cristopher" : 322414124 ,
+  "Rebecca" : 925232658 ,
+  "Bob" : 126521414 ,
+}
+print(users)
+users["Bob"] = 33445566
+print(users)
+users.update({"Normund" : 421412412})
+print(users)
+users.pop("Bob")
+print(users)
